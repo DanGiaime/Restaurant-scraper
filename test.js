fb804dfc66c66cc7ea2c1a97fa6dd081b3621224
Dan Giaime <dsg6874@g.rit.edu>
2019-12-10 16:33:18 -0600

    Sync

--- test.js
+++ test.js
@@ -7,39 +7,33 @@ var parse = require('csv-parse')
 fs.readFile("/Users/work/Downloads/POS_MS.csv", function (err, fileData) {
   parse(fileData, {columns: false, trim: true}, function(err, rows) {
 	// Your CSV data is in an array of arrys passed to this callback as rows.
-	let allWebsitePromises = findAllWebsites(rows);
-	let allWebsites = {};
-	allWebsitePromises.forEach(async websitePromise => {
-		await websitePromise.then(data => {
-			allWebsites[data.name] = data.site;
-		}).catch(test => console.log(test));
-	});
-	Promise.all(allWebsitePromises).then(() => console.log(allWebsites));
-
+	findAllWebsites(rows).then(websites => console.log(websites));
+	
   })
 })
 
 
-let findAllWebsites = (csvData) => {
+let findAllWebsites = async (csvData) => {
 	let websites = [];
-	for(let i = 0; i < 20; i++) { //csvData.length; i++) {
+	console.log("Restaurant websites to be found: " + csvData.length);
+	for(let i = 0; i < csvData.length; i++) {
 		let currRestaurantName = csvData[i][0];
-		// console.log(currRestaurantName);
-		websites.push(tabLoader(currRestaurantName));
+		websites.push(await tabLoader(currRestaurantName));
+		if(i%10 == 0) {
+			console.log(websites[i]);
+		}
 	}
 	return websites;
 }
 
 
 let tabLoader = async (restaurantName) => {
-	// console.log(restaurantName);
 	const tab = await nick.newTab()
 	await tab.open(`www.google.com/search?q=${restaurantName}+chicago`)
 
 	await tab.untilVisible("body") // Make sure we have loaded the page
 
 	await tab.inject("http://code.jquery.com/jquery-3.2.1.min.js") // We're going to use jQuery to scrape
-	console.log(restaurantName);
 
 	const googleData = await tab.evaluate((arg, callback) => {
 		callback(null, {
@@ -50,5 +44,4 @@ let tabLoader = async (restaurantName) => {
 	googleData.name = restaurantName;
 
 	return googleData;
-
 };
