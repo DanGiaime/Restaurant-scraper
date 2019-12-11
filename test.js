74f99c7de6c7ff1d9f611e55d7f96eb7d0683708
Dan Giaime <dsg6874@g.rit.edu>
2019-12-10 19:55:31 -0600

    Async improved with tab pool

--- test.js
+++ test.js
@@ -3,43 +3,72 @@ const nick = new Nick()
 
 const fs = require('fs')
 var parse = require('csv-parse')
+const genericPool = require("generic-pool");
+
+/**
+ * Step 1 - Create pool using a factory object
+ */
+const factory = {
+  create: function() {
+    return nick.newTab();
+  },
+  destroy: function(tab) {
+    tab.close();
+  }
+};
+
+const opts = {
+  max: 5, // maximum size of the pool
+  min: 5 // minimum size of the pool
+};
+
+const myPool = genericPool.createPool(factory, opts);
 
 fs.readFile("/Users/work/Downloads/POS_MS.csv", function (err, fileData) {
-  parse(fileData, {columns: false, trim: true}, function(err, rows) {
-	// Your CSV data is in an array of arrys passed to this callback as rows.
-	let allWebsitePromises = findAllWebsites(rows);
-	let allWebsites = {};
-	allWebsitePromises.forEach(async websitePromise => {
-		await websitePromise.then(data => {
-			allWebsites[data.name] = data.site;
-		}).catch(test => console.log(test));
-	});
-	Promise.all(allWebsitePromises).then(() => console.log(allWebsites));
+	parse(fileData, { columns: false, trim: true }, function (err, rows) {
+		// Your CSV data is in an array of arrys passed to this callback as rows.
+		let allWebsites = {};
+		let allWebsitePromises = findAllWebsites(rows);
+		Promise.all(allWebsitePromises).then(promises => {
+			console.log("LENGTH: " + promises.length)
+			promises.forEach(
+				data => {
+					allWebsites[data.name] = data.site
+				}
+			);
+		})
+		.then(() => console.log(allWebsites)).catch(err => console.log(err));
+	})
 
-  })
 })
-
 
 let findAllWebsites = (csvData) => {
 	let websites = [];
-	for(let i = 0; i < 20; i++) { //csvData.length; i++) {
+	for (let i = 0; i < csvData.length; i++) {
 		let currRestaurantName = csvData[i][0];
-		// console.log(currRestaurantName);
-		websites.push(tabLoader(currRestaurantName));
+		console.log("Checking " + currRestaurantName)
+		websites.push(myPool.acquire().then(tab => findWebsiteData(currRestaurantName, tab)));
 	}
 	return websites;
 }
 
+// let createTabPools = async () => {
+// 	let tabs = [];
+// 	for (let i = 0; i < 20; i++) {
+// 		tabs.push(await nick.newTab());
+// 		console.log("Made tab " + i)
+// 	}
+// 	return tabs;
+// };
 
-let tabLoader = async (restaurantName) => {
+
+let findWebsiteData = async (restaurantName, tab) => {
 	// console.log(restaurantName);
-	const tab = await nick.newTab()
 	await tab.open(`www.google.com/search?q=${restaurantName}+chicago`)
 
-	await tab.untilVisible("body") // Make sure we have loaded the page
+	await tab.untilVisible(".r") // Make sure we have loaded the page
 
 	await tab.inject("http://code.jquery.com/jquery-3.2.1.min.js") // We're going to use jQuery to scrape
-	console.log(restaurantName);
 
 	const googleData = await tab.evaluate((arg, callback) => {
 		callback(null, {
@@ -49,6 +78,8 @@ let tabLoader = async (restaurantName) => {
 
 	googleData.name = restaurantName;
 
+	myPool.release(tab);
+	console.log(googleData);
 	return googleData;
 
 };
